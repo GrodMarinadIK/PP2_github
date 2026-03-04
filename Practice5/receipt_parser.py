286af665803c6885ba5e03fea640a43ad2e28567
Miruansani Zhanspayev <mikulovermiruan@gmail.com>
2026-03-04 09:48:42 +0500

    changed receipt_parser.py

--- Practice5/receipt_parser.py
+++ Practice5/receipt_parser.py
@@ -9,21 +9,25 @@ path_to_receipt_json = os.path.join(current_dir, 'receipt.json')
 with open(path_to_raw_txt, 'r', encoding='utf-8') as f:
     content = f.read()
 
-# 1. Извлекаем дату и время
+# 1. Extract date and time
+#                          DD.MM.YYYY           HH:MM:SS
 dt_match = re.search(r"(\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}:\d{2})", content)
 date_time = dt_match.group(1) if dt_match else "Unknown"
 
-# 2. Извлекаем метод оплаты
+# 2.Extract type of payment
 payment_method = "Банковская карта" if "Банковская карта" in content else "Наличные"
 
-# 3. Извлекаем товары и цены
+# 3. Extract products and prices
 products = []
+    # \d+\.\n - position, (.*?) - name of a product, \n.*?\n - skipping information, ([\d\s]+,00) - price,
+    # re.DOTALL - is for dot to see line breaks
 items = re.findall(r"\d+\.\n(.*?)\n.*?\n([\d\s]+,00)", content, re.DOTALL)
 
 total_calculated = 0
 for name, price_str in items:
     clean_name = name.replace('\n', ' ').strip()
-    # Убираем пробелы между тысячами и меняем запятую на точку для float
+    # Deleting spaces between thousands and replacing comma with dot for float
+    # e.g. 1 500 -> 1500; 1500,00 -> 1500.00;
     price_val = float(price_str.replace(' ', '').replace(',', '.'))
     
     products.append({
@@ -32,7 +36,7 @@ for name, price_str in items:
     })
     total_calculated += price_val
 
-# Финальный словарь БЕЗ цитат
+# Final dictionary
 receipt_data = {
     "store": "EUROPHARMA",
     "date_time": date_time,
@@ -40,7 +44,7 @@ receipt_data = {
     "items": products,
     "total_sum": total_calculated
 }
-
+# ensure_ascii=False - to see Cyrilic not the ascii references
 print(json.dumps(receipt_data, indent=4, ensure_ascii=False))
 
 with open(path_to_receipt_json, 'w', encoding='utf-8') as f:
